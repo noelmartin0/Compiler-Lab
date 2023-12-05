0620eafac53a9e9a4dd78148b6dd9ba916285090
noelmartin0 <noelmartin0@gmail.com>
2023-12-05 20:35:17 +0530

    add new code

---
+++ Constant Propagation/cp.c
@@ -0,0 +1,94 @@
+#include<stdio.h>
+#include<ctype.h>
+#include<string.h>
+int z = 0;
+struct constant
+{
+    char var;
+    int value;
+}con[10];
+void findConstant(char exp[20])
+{
+    if(isdigit(exp[3]))
+    {
+        con[z].var = exp[1];
+        con[z].value = exp[3] - 48;
+        z++;
+    }
+}
+void main()
+{
+    int n,flag=0,count=1,op1,op2;
+    char exp[20][20];
+    printf("\nEnter the number of expression: \n");
+    scanf("%d",&n);
+    printf("\nEnter the expression in quadruple form: \n");
+    for(int i = 0; i < n; i++)
+        scanf("%s",exp[i]);
+    printf("\nOptimized Code:\n");
+    for(int i = 0; i < n; i++)
+    {
+        if(exp[i][0] == '=')
+            findConstant(exp[i]);
+    }
+    for(int i = 0; i < n; i++)
+    {
+        if(!(exp[i][0] == '='))
+        {
+            printf("%c ",exp[i][0]);
+            char operator = exp[i][0];
+            for(int j=1; exp[i][j] != '\0'; j++)
+            {
+                flag = 0;
+                for(int k=0; k<z; k++)
+                {
+                    if(con[k].var == exp[i][j])
+                    {
+                        printf("%d ",con[k].value);
+                        flag = 1;
+                        if(count == 1)
+                        {
+                            op1 = con[k].value;
+                            count++;
+                        }
+                        else
+                        {
+                            op2 = con[k].value;
+                            count = 1;
+                        }
+                        break;
+                    }
+                }
+                if(flag == 0)
+                    printf("%c ",exp[i][j]);
+                if(exp[i][j+1] == '\0')
+                {
+                    switch(operator)
+                    {
+                        case '+': op1=op1+op2;
+                                  con[z].var = exp[i][j];
+                                  con[z].value = op1;
+                                  z++;
+                                  break;
+                        case '-': op1=op1-op2;
+                                  con[z].var = exp[i][j];
+                                  con[z].value = op1;
+                                  z++;
+                                  break;
+                        case '*': op1=op1*op2;
+                                  con[z].var = exp[i][j];
+                                  con[z].value = op1;
+                                  z++;
+                                  break;
+                        case '/': op1=op1/op2;
+                                  con[z].var = exp[i][j];
+                                  con[z].value = op1;
+                                  z++;
+                                  break;
+                    }
+                }
+            }
+            printf("\n");
+        }
+    }
+}
---
+++ Three Address Code/tac.c
@@ -0,0 +1,71 @@
+#include<stdio.h>
+#include<ctype.h>
+#include<string.h>
+struct expression{
+    char op1,op2,operator,result;
+}expr[10];
+void main()
+{
+    int k=0,flag=0;
+    char input[20],res[20],temp='x';
+    printf("\nEnter the expression: ");
+    scanf("%s",input);
+    for(int i=2; input[i] != '\0'; i++)
+    {
+        if(flag == 2)
+        {
+            expr[k].op1 = expr[k-1].result;
+            flag=1;
+            if(!isalpha(input[i]))
+            {
+                expr[k].operator = input[i];
+                continue;
+            }
+        }
+        else if(isalpha(input[i]) && flag == 0)
+        {
+            expr[k].op1 = input[i];
+            flag = 1;
+        }
+        else if(isalpha(input[i]))
+        {
+            expr[k].op2 = input[i];
+            expr[k].result = temp;
+            temp++;
+            k++;
+            flag = 2;
+        }
+        else
+        {
+            expr[k].operator = input[i];
+        }
+    }
+    expr[k].result = input[0];
+    expr[k].op1 = expr[k-1].result;
+    k++;
+    printf("\nIntermediate Code\n");
+    for(int i=0; i<k; i++)
+    {
+        printf("%c = %c %c %c\n",expr[i].result,expr[i].op1,expr[i].operator,expr[i].op2);
+    }
+    printf("\nTRIPLE\n");
+    for(int i = 0; i<k; i++)
+    {
+        if(i == k-1)
+        {
+            printf("[%d] = [%d]\n",i,i-1);
+            break;
+        }
+        else if(i != 0)
+        {
+            printf("[%d] = %c [%d] %c\n",i,expr[i].operator,i-1,expr[i].op2);
+        }
+        else
+            printf("[%d] = %c %c %c\n",i,expr[i].operator,expr[i].op1,expr[i].op2);
+    }
+    printf("\nQUADRUPLE\n");
+    for(int i = 0; i<k; i++)
+    {
+        printf("[%d] = %c %c %c %c\n",i,expr[i].operator,expr[i].op1,expr[i].op2,expr[i].result);
+    }
+}
