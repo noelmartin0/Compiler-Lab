9b3d507d07a6ada670238f4219da268e975e90b2
noelmartin0 <noelmartin0@gmail.com>
2023-12-05 10:20:08 +0530

    new file:   Recursive Descent Parser/rdp.c

---
+++ Recursive Descent Parser/rdp.c
@@ -0,0 +1,66 @@
+#include<stdio.h>
+void E();
+void E1();
+void T();
+void T1();
+void F();
+char a[20];
+int z = 0, flag = 0;
+int main()
+{
+    printf("\nEnter a string: ");
+    scanf("%s",a);
+    E();
+    if(a[z] == 0 && flag == 0)
+        printf("\nValid");
+    else
+        printf("\nInvalid");
+    return 0;
+}
+
+void E()
+{
+    T();
+    E1();
+}
+
+void E1()
+{
+    if(a[z] == '+')
+    {
+        z++;
+        T();
+        E1();
+    }    
+}
+
+void T()
+{
+    F();
+    T1();
+}
+
+void T1()
+{
+    if(a[z] == '*')
+    {
+        z++;
+        F();
+        T1();
+    }
+}
+
+void F()
+{
+    if(a[z] == '(')
+    {
+        z++;
+        E();
+        if(a[z] == ')')
+            z++;
+    }
+    else if(a[z] == 'i')
+        z++;
+    else
+        flag = 1;
+}
---
+++ Recursive Descent Parser/readme.md
@@ -0,0 +1,8 @@
+# NOTE
+---
+### Grammar for mentioned program is given below
+- E  -> TE'
+- E' -> +TE'
+- T  -> FT'
+- T' -> *FT'
+- F  -> (E)|id
